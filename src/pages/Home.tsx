fc823091eddd847ae9a9be4e7f40bd190fa75a1f
kiran04-code <kr551344@gmail.com>
2026-08-05 08:48:34 +0530

    Day:1-GithubAuthentication-start:10%

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { FaGithub } from "react-icons/fa";
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-black text-2xl">Home pages</h1>
+    <div className="flex justify-center h-screen w-full items-center">
+      <button type="" className="flex justify-center items-center gap-4 bg-black text-white p-3 rounded-xl">
+        <FaGithub /> Login with Github
+      </button>
     </div>
   )
 }
